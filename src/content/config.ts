974492456f8e9dd02dd9efd154b54b747d2ea04d
unfold-potential <archana19dhankar@gmail.com>
2026-06-01 18:21:31 +0300

    pinned logic

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
     tags: z.array(z.string()).optional(),
+    pinned: z.boolean().optional(),
   }),
 });
 
